88fc727e922205245b81e5316e949370ebaa7fc9
Jay Leimer <41790928+CodingJinxx@users.noreply.github.com>
2019-12-29 20:49:28 +0100

    Nonogram Game working.

--- flaskr/application.py
+++ flaskr/application.py
@@ -1,7 +1,53 @@
-from flask import Flask
-import sqlite3
-app= Flask(__name__)
+from flask import (Flask, render_template, redirect,  request)
+from flask_login import (LoginManager, login_required, login_user, logout_user)
+from user import (User)
+from database import (InitDatabase)
+import os
 
-@app.route('/')
+#----App Configurations----
+app=   Flask(__name__)
+app.secret_key = "t1KhONWYGNEtZ0sh"
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+if(os.getenv("INIT_DATABASE") == "True"):
+    InitDatabase()
+    os.putenv("INIT_DATABASE", "False")
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view
+#--------------------------
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route('/', methods=["GET"])
 def hello_world():
-    return 'Hello, Word!'
+    return redirect('/register')
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == 'POST':
+        return 0
+    else:
+        return render_template("register.html")
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route('/profile', methods=["GET"])
+@login_required
+def profile():
+    return render_template("profile.html", Username="Jinxx", Stars_collected="6")
+
+@app.route('/level', methods=["GET"])
+@login_required
+def level():
+    return render_template("level.html")
+
+if __name__ == "__main__":
+    app.run()
---
+++ flaskr/static/js/game.js
@@ -0,0 +1,293 @@
+var Grid; // [x][y]
+var Solution; // [x][y]
+var Lives;
+var GameOver;
+
+function generateGrid(size)
+{       
+    Lives = 3;
+    GameOver = false;
+
+    
+    Grid = createArray(size, size);
+    Solution = generatePuzzle(size);
+    for(let x = 0; x < size; x++)
+    {
+        let numbers = document.createElement("ul");
+        for(let j = 0; j < Solution.xNumber[x].length; j++)
+        {
+            let number = document.createElement("li");
+            number.appendChild(document.createTextNode(Solution.xNumber[x][j]));
+            numbers.appendChild(number);
+        }
+
+        let a = document.createElement("div");
+        a.appendChild(numbers);
+        let b = document.createElement("div");
+        b.appendChild(a);
+        document.getElementById("xNumbers").appendChild(b);
+    }
+
+    for(let y = 0; y < size; y++)
+    {
+        let numbers = document.createElement("ul");
+        for(let j = 0; j < Solution.yNumber[y].length; j++)
+        {
+            let number = document.createElement("li");
+            number.appendChild(document.createTextNode(Solution.yNumber[y][j]));
+            numbers.appendChild(number);
+        }
+        let a = document.createElement("div");
+        a.appendChild(numbers);
+        let b = document.createElement("div");
+        b.appendChild(a);
+        document.getElementById("yNumbers").appendChild(b);
+    }
+
+    for(let x = 0; x < size; x++)
+    {
+        let row = document.createElement("div");
+        row.className = "Row";
+        for(let y = 0; y < size; y++)
+        {
+            let cell = document.createElement("div");
+            cell.className = "Cell";
+            cell.id = x + "|" + y;
+            cell.oncontextmenu = () => {return false};
+            cell.onmousedown = cellClicked;
+            cell.onmouseenter = cellMouseEnter;
+            cell.onmouseleave = cellMouseLeave;
+            row.appendChild(cell);
+        }
+        document.getElementById("gridCanvas").appendChild(row);
+    }
+}
+
+function setCell(x, y, value)
+{
+    if(value != -2 && value != -1 && value != 0 && value != 1)
+    {
+        alert("ERROR");
+        return false;
+    }
+    Grid[x][y] = value;
+    let cell = document.getElementById(x + "|" + y);
+    switch(value)
+    {
+        case 1:
+            toggleClass(cell, "Cell-O", true)
+            toggleClass(cell, "Cell-X", false)
+            toggleClass(cell, "Cell-Red", false)
+            break;
+        case -1:
+            toggleClass(cell, "Cell-X", true)
+            toggleClass(cell, "Cell-O", false)
+            toggleClass(cell, "Cell-Red", false)
+            break;
+        case 0:
+            toggleClass(cell, "Cell-O", false)
+            toggleClass(cell, "Cell-X", false)
+            toggleClass(cell, "Cell-Red", false)
+        case -2:
+            toggleClass(cell, "Cell-O", false)
+            toggleClass(cell, "Cell-X", false)
+            toggleClass(cell, "Cell-Red", true)
+            break;
+    }
+}
+
+function checkGrid()
+{
+    for(let i = 0; i < Grid.length; i++)
+    {
+        for(let j = 0; j < Grid[0].length; j++)
+        {        
+            if(Grid[i][j] == -1 || Grid[i][j] == 1)
+            {
+                console.log(Grid[i][j] + " == " + Solution.grid[i][j]);
+                if(Grid[i][j] != Solution.grid[i][j])
+                {
+                    setCell(i, j, -2)
+                }
+            }
+        }
+    }
+}
+
+function isCellCorrect(x,y, value)
+{
+    if(value == Solution.grid[x][y])
+    {
+        return true;
+    }
+    return false;
+}
+
+function cellClicked(event)
+{
+    position = this.id.split("|").map(function(item)
+    {
+        return parseInt(item, 10);
+    });
+    toggleClass(this, "Cell-Red", false);
+    switch(event.which)
+    {
+        case 1:
+            if(!isCellCorrect(position[0], position[1], 1))
+            {
+                setCell(position[0], position[1], -2);
+            }
+            else
+            {
+                setCell(position[0], position[1], 1);
+            }
+            break;
+
+        case 3: 
+            if(toggleClass(this, "Cell-X"))
+            {
+                Grid[position[0]][position[1]] = -1;
+            }
+            else
+            {
+                Grid[position[0]][position[1]] = 0;
+            }
+            toggleClass(this, "Cell-O", false);
+            break;
+        default:
+    }
+}
+
+function cellMouseEnter()
+{
+    toggleClass(this, "Cell-Hover", true);
+}
+
+function cellMouseLeave()
+{
+    toggleClass(this, "Cell-Hover", false);
+}
+
+function generatePuzzle(size)
+{
+    xNumber = createArray(size, 0);
+    yNumber = createArray(size, 0);
+    grid = createArray(size, size);
+
+    // --------- GRID ---------
+    // Each Row/y
+    for(let y = 0; y < size; y++)
+    {
+        // Each Column/x
+        for(let x = 0; x < size; x++)
+        {
+            let chance = Math.random()
+            if(chance < 0.4)
+            {
+                grid[x][y] = -1;
+            }
+            else
+            {
+                grid[x][y] = 1;
+            }
+        }
+    }
+
+    // ---------  XNUMBER ---------
+    // Each Column - xNumber
+    let totalCounter = 0;
+    let blockCounter = 0;
+    for(let x = 0; x < size; x++)
+    {
+        if(blockCounter > 0)
+        {
+            xNumber[x - 1].push(blockCounter);
+        }
+        totalCounter = 0;
+        blockCounter = 0;
+        while(totalCounter < size)
+        {
+            if(grid[x][totalCounter] == 1)
+            {
+                blockCounter++;
+            }
+            else if(grid[x][totalCounter] == -1 && blockCounter != 0)
+            {
+                xNumber[x].push(blockCounter);
+                blockCounter = 0;
+            }
+            totalCounter++;
+        }
+    }
+    if(blockCounter > 0)
+    {
+        xNumber[size - 1].push(blockCounter);
+    }
+
+    // --------- YNUMBER ---------
+    // Each Row - yNumber
+    totalCounter = 0;
+    blockCounter = 0;
+    for(let y = 0; y < size; y++)
+    {
+        if(blockCounter > 0)
+        {
+            yNumber[y - 1].push(blockCounter);
+        }
+        totalCounter = 0;
+        blockCounter = 0;
+        while(totalCounter < size)
+        {
+            if(grid[totalCounter][y] == 1)
+            {
+                blockCounter++;
+            }
+            else if(grid[totalCounter][y] == -1 && blockCounter != 0)
+            {
+                yNumber[y].push(blockCounter);
+                blockCounter = 0;
+            }
+            totalCounter++;
+        }
+    }
+    if(blockCounter > 0)
+    {
+        yNumber[size - 1].push(blockCounter);
+    }
+    return {xNumber, yNumber, grid};
+}
+
+function show()
+{
+    for(let x = 0; x < Solution.grid.length; x++)
+    {
+        for(let y = 0; y < Solution.grid[0].length; y++)
+        {
+            setCell(x, y, Solution.grid[x][y]);
+        }
+    }
+}
+
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
+function toggleClass(elem, theClass, newState) {
+    var matchRegExp = new RegExp('(?:^|\\s)' + theClass + '(?!\\S)', 'g');
+    var add=(arguments.length>2 ? newState : (elem.className.match(matchRegExp) == null));
+    elem.className=elem.className.replace(matchRegExp, ''); // clear all
+    if (add) 
+    {
+        elem.className += ' ' + theClass;
+        return true;
+    }
+    return false;
+}
