85d13ccc21802d6d21147c6aac562ef383309f98
Jay Leimer <41790928+CodingJinxx@users.noreply.github.com>
2019-12-30 20:00:34 +0100

    Temp

--- flaskr/application.py
+++ flaskr/application.py
@@ -1,42 +1,66 @@
-from flask import (Flask, render_template, redirect,  request)
-from flask_login import (LoginManager, login_required, login_user, logout_user)
-from user import (User)
-from database import (InitDatabase)
-import os
+from flask import (Flask, render_template, redirect,  request, url_for)
+from flask_sqlalchemy import (SQLAlchemy)
+from flask_login import (LoginManager, login_required, login_user, current_user, logout_user, UserMixin)
+from werkzeug.security import (generate_password_hash, check_password_hash)
+import io
 
 #----App Configurations----
-app=   Flask(__name__)
-app.secret_key = "t1KhONWYGNEtZ0sh"
+app =   Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db';
+app.config['SECRET_KEY'] = "t1KhONWYGNEtZ0sh"
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-if(os.getenv("INIT_DATABASE") == "True"):
-    InitDatabase()
-    os.putenv("INIT_DATABASE", "False")
-
-
+db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view
 #--------------------------
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    passwordhash = db.Column(db.String(64), nullable=False)
+    stars = db.Column(db.Integer, default=0, nullable=False)
+    
+    def check_password(self, password):
+        return check_password_hash(self.passwordhash, password)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(int(user_id))
 
+
+#---------- ROUTES ----------
 @app.route('/', methods=["GET"])
-def hello_world():
+def index():
     return redirect('/register')
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
-        return 0
+        username = str(request.)
+        print(username)
+        password = str(request.args.get("password"))
+        print(password)
+        confirm = str(request.args.get("confirm"))
+        print(confirm)
+
+        if(len(username) == 0 or len(password) == 0 or len(confirm) == 0):
+            return "Invalid Length"
+
+        if(password != confirm):
+            return "Passwords dont match"
+
+        passwordhashed = generate_password_hash(password)
+        return render_template("profile.html")
     else:
         return render_template("register.html")
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        redirect(url_for(index))
+    
     return render_template("login.html")
 
 @app.route('/profile', methods=["GET"])
@@ -49,5 +73,4 @@ def profile():
 def level():
     return render_template("level.html")
 
-if __name__ == "__main__":
-    app.run()
+
